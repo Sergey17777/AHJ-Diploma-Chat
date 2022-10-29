2e0cfd8000d9df6552e9a49221d7114a22e9f3b1
Сергей Григорьев <92942323+Sergey17777@users.noreply.github.com>
2022-10-29 16:36:56 +0500

    Delete app directory

--- app/src/js/__tests__/demo.test.js
+++
@@ -1,7 +0,0 @@
-import demo from '../demo';
-
-test('test', () => {
-  const received = 0;
-  const expected = demo(received);
-  expect(expected).toEqual(received);
-});
--- app/src/js/demo.js
+++
@@ -1,3 +0,0 @@
-export default function demo(value) {
-  return value;
-}
